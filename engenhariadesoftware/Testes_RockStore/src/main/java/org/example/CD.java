03da89058b1b0d9ccbcfc61a1ef1a8d0319b813d
luiz-andrade1 <luizfelipequeirosandrade@gmail.com>
2026-05-04 18:56:11 -0300

    Adiciona classe CD

--- engenhariadesoftware/Testes_RockStore/src/main/java/org/example/CD.java
+++ engenhariadesoftware/Testes_RockStore/src/main/java/org/example/CD.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class CD {
+
+    private String genero;
+    private Integer numeroFaixas;
+    private String artista;
+
+    public CD (String genero, Integer numeroFaixas, String artista){
+        this.genero = genero;
+        this.numeroFaixas = numeroFaixas;
+        this.artista = artista;
+    }
+
+    public CD(){}
+
+    public String getGenero(){
+        return genero;
+    }
+    public void setGenero(String genero){this.genero = genero;}
+    public Integer getNumeroFaixas(){return numeroFaixas;}
+    public void setNumeroFaixas(Integer numeroFaixas){this.numeroFaixas = numeroFaixas;}
+    public String getArtista(){return artista;}
+    public void setArtista(String artista){this.artista = artista;}
 }
