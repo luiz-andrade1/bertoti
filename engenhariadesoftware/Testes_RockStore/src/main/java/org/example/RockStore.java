9c8779206a740ae82eaf1c563d0269dc425bc8ac
luiz-andrade1 <luizfelipequeirosandrade@gmail.com>
2026-05-04 20:21:45 -0300

    Adiciona a funcionalidade de cadastrar Camisas em uma lista

--- engenhariadesoftware/Testes_RockStore/src/main/java/org/example/RockStore.java
+++ engenhariadesoftware/Testes_RockStore/src/main/java/org/example/RockStore.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.util.List;
+import java.util.LinkedList;
 
 public class RockStore {
 
@@ -12,5 +14,23 @@ public class RockStore {
 
     public RockStore(){}
 
+    private List<Camisa> camisas = new LinkedList<Camisa>();
 
+    public List<Camisa> getCamisas(){
+        return camisas;
+    }
+
+    public void cadastrarCamisa(Camisa camisa){
+        camisas.add(camisa);
+    }
+
+    public List<Camisa> buscarCamisasMarca(String marca){
+        List<Camisa> encontradas = new LinkedList<Camisa>();
+        for(Camisa camisa:camisas){
+            if (camisa.getMarca().equals(marca))
+                encontradas.add(camisa);
+        }
+        return encontradas;
+
+    }
 }
